d718c4e996fce1980e7556b5f2179456f07c1af7
Xtian2108 <christian_2108@live.com>
2022-02-25 22:39:44 -0500

    Update Scripts

--- Assets/Web3Unity/Scripts/Prefabs/ERC1155/ERC1155BalanceOfExample.cs
+++ Assets/Web3Unity/Scripts/Prefabs/ERC1155/ERC1155BalanceOfExample.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Numerics;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class ERC1155BalanceOfExample : MonoBehaviour
 {
     async void Start()
     {
-        string chain = "avalanche";
-        string network = "testnet";
-        string contract = "0xbDF2d708c6E4705824dC024187cd219da41C8c81";
-        string account = "0xdD4c825203f97984e7867F11eeCc813A036089D1";
-        string tokenId = "2";
+        string chain = "ethereum";
+        string network = "rinkeby";
+        string contract = "0x88b48f654c30e99bc2e4a1559b4dcf1ad93fa656";
+        string account = PlayerPrefs.GetString("Account");
+        string tokenId = "68097713221331394305288366584829292296340766581605097826805820294879527305217";
 
         BigInteger balanceOf = await ERC1155.BalanceOf(chain, network, contract, account, tokenId);
         print(balanceOf);
+        if(balanceOf > 0)
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            go.transform.position = Vector3.zero;
+        }
     }
 }
